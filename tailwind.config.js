160c4d27b41f4b15b290e21175d149fafe9029f9
rushomrong <rushomarak@gmail.com>
2024-08-20 01:57:12 +0600

    header section added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,16 @@
-import daisyui from "./node_modules/daisyui";
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*"],
   theme: {
-    fontFamily: {
-      poppins: "Poppins", "sans-serif";
-    },
-    extend: {},
-  },
-  plugins: [daisyui],
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
 
-  daisyui: {
-    themes: false, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
-    darkTheme: "dark", // name of one of the included themes for dark mode
-    base: true, // applies background color and foreground color for root element by default
-    styled: true, // include daisyUI colors and design decisions for all components
-    utils: true, // adds responsive and modifier utility classes
-    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
-    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
-    themeRoot: ":root", // The element that receives theme color CSS variables
+      colors: {
+        "main-btn": "#E76F51",
+      },
+    },
   },
+  plugins: [require("daisyui")],
 };
