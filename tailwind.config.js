ea61886c44018ac17a1e86a919d32096d6c0692a
rushomrong <rushomarak@gmail.com>
2024-08-19 23:15:56 +0600

    Biker zone - Bike dekho project initialized with daisy UI and tailwind css setup done

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+};
